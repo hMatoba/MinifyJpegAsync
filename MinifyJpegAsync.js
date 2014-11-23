a8a459b98283356322a8e2dd9e137334cb94a6d5
hMatoba <g838.mahiroo@gmail.com>
2014-11-23 21:14:10 +0900

    Added "use strict" and refactored.

--- MinifyJpegAsync.js
+++ MinifyJpegAsync.js
@@ -15,8 +15,8 @@
  * MinifyJpeg.encode64() - convert array to base64encoded string
  */
 
-var MinifyJpegAsync = (function()
-{
+var MinifyJpegAsync = (function () {
+    "use strict";
     var that = {};
 
     that.KEY_STR = "ABCDEFGHIJKLMNOP" +
@@ -27,64 +27,51 @@ var MinifyJpegAsync = (function()
 
     that.SOF = [192, 193, 194, 195, 197, 198, 199, 201, 202, 203, 205, 206, 207];
 
-    that.minify = function(image, new_size, callback)
-    {
-        var imageObj = new Image();
+    that.minify = function (image, new_size, callback) {
+        var imageObj = new Image(),
+            rawImage = [],
+            imageStr = "";
 
-        if (image instanceof ArrayBuffer)
-        {
-            if (image[0] == 255 && image[1] == 216)
-            {
-                var rawImage = [];
-                for (var i=0; i<image.byteLength; i++)
-                {
+        if (image instanceof ArrayBuffer) {
+            if (image[0] == 255 && image[1] == 216) {
+                for (var i = 0; i < image.byteLength; i++) {
                     rawImage[i] = image[i];
                 }
-                var imageStr = "data:image/jpeg;base64," + that.encode64(rawImage);
-            }
-            else
-            {
+                imageStr = "data:image/jpeg;base64," + that.encode64(rawImage);
+            } else {
                 throw "MinifyJpeg.minify got a not JPEG data";
             }
-        }
-        else if (typeof(image) == "string")
-        {
-            if (!image.match("data:image/jpeg;base64,"))
-            {
+        } else if (typeof (image) == "string") {
+            if (!image.match("data:image/jpeg;base64,")) {
                 throw "MinifyJpeg.minify got a not JPEG data";
+            } else {
+                rawImage = that.decode64(image.replace("data:image/jpeg;base64,", ""));
+                imageStr = image;
             }
-            else
-            {
-                var rawImage = that.decode64(image.replace("data:image/jpeg;base64,", ""));
-                var imageStr = image;
-            }
-        }
-        else
-        {
+        } else {
             throw "First argument must be 'DataURL string' or ArrayBuffer.";
         }
 
-        imageObj.onload = function()
-        {
-            var segments = that.slice2Segments(rawImage);
-            var NEW_SIZE = parseInt(new_size);
-            var size = that.imageSizeFromSegments(segments);
-            var chouhen = (size[0] >= size[1]) ? size[0] : size[1];
-            if (chouhen < NEW_SIZE)
-            {
+        imageObj.onload = function () {
+            var segments = that.slice2Segments(rawImage),
+                NEW_SIZE = parseInt(new_size),
+                size = that.imageSizeFromSegments(segments),
+                chouhen = (size[0] >= size[1]) ? size[0] : size[1];
+            var exif,
+                resized,
+                newImage;
+            
+            if (chouhen < NEW_SIZE) {
                 return new Uint8Array(rawImage);
             }
 
-            var exif = that.getExif(segments);
-            var resized = that.resize(imageObj, segments, NEW_SIZE);
+            exif = that.getExif(segments);
+            resized = that.resize(imageObj, segments, NEW_SIZE);
 
-            if (exif.length)
-            {
-                var newImage = that.insertExif(resized, exif);
-            }
-            else
-            {
-                var newImage = new Uint8Array(that.decode64(resized.replace("data:image/jpeg;base64,", "")));
+            if (exif.length) {
+                newImage = that.insertExif(resized, exif);
+            } else {
+                newImage = new Uint8Array(that.decode64(resized.replace("data:image/jpeg;base64,", "")));
             }
 
             callback(newImage);
@@ -93,37 +80,32 @@ var MinifyJpegAsync = (function()
 
     };
 
-    that.getImageSize = function(imageArray)
-    {
+    that.getImageSize = function (imageArray) {
         var segments = that.slice2Segments(imageArray);
         return that.imageSizeFromSegments(segments);
     };
 
-    that.slice2Segments = function(rawImageArray)
-    {
+    that.slice2Segments = function (rawImageArray) {
         var head = 0,
             segments = [];
-
-        while (1)
-        {
-            if (rawImageArray[head] == 255 & rawImageArray[head + 1] == 218)
-            {
+        var length,
+            endPoint,
+            seg;
+        
+        while (1) {
+            if (rawImageArray[head] == 255 && rawImageArray[head + 1] == 218) {
                 break;
             }
-            if (rawImageArray[head] == 255 & rawImageArray[head + 1] == 216)
-            {
+            if (rawImageArray[head] == 255 && rawImageArray[head + 1] == 216) {
                 head += 2;
-            }
-            else
-            {
-                var length = rawImageArray[head + 2] * 256 + rawImageArray[head + 3],
-                    endPoint = head + length + 2,
-                    seg = rawImageArray.slice(head, endPoint);
+            } else {
+                length = rawImageArray[head + 2] * 256 + rawImageArray[head + 3],
+                endPoint = head + length + 2,
+                seg = rawImageArray.slice(head, endPoint);
                 segments.push(seg);
                 head = endPoint;
             }
-            if (head > rawImageArray.length)
-            {
+            if (head > rawImageArray.length) {
                 break;
             }
         }
@@ -131,23 +113,22 @@ var MinifyJpegAsync = (function()
         return segments;
     };
 
-    that.imageSizeFromSegments = function(segments)
-    {
-        for  (var x=0; x<segments.length; x++)
-        {
-            var seg = segments[x];
-            if (that.SOF.indexOf(seg[1]) >= 0)
-            {
-                var height = seg[5] * 256 + seg[6],
-                    width = seg[7] * 256 + seg[8];
+    that.imageSizeFromSegments = function (segments) {
+        var seg,
+            width,
+            height;
+        for (var x = 0; x < segments.length; x++) {
+            seg = segments[x];
+            if (that.SOF.indexOf(seg[1]) >= 0) {
+                height = seg[5] * 256 + seg[6],
+                width = seg[7] * 256 + seg[8];
                 break;
             }
         }
         return [width, height];
     };
 
-    that.encode64 = function(input)
-    {
+    that.encode64 = function (input) {
         var output = "",
             chr1, chr2, chr3 = "",
             enc1, enc2, enc3, enc4 = "",
@@ -164,22 +145,22 @@ var MinifyJpegAsync = (function()
             enc4 = chr3 & 63;
 
             if (isNaN(chr2)) {
-               enc3 = enc4 = 64;
+                enc3 = enc4 = 64;
             } else if (isNaN(chr3)) {
-               enc4 = 64;
+                enc4 = 64;
             }
 
             output += that.KEY_STR.charAt(enc1) +
-                              that.KEY_STR.charAt(enc2) +
-                              that.KEY_STR.charAt(enc3) +
-                              that.KEY_STR.charAt(enc4);
+                that.KEY_STR.charAt(enc2) +
+                that.KEY_STR.charAt(enc3) +
+                that.KEY_STR.charAt(enc4);
             chr1 = chr2 = chr3 = "";
             enc1 = enc2 = enc3 = enc4 = "";
         } while (i < input.length);
         return output;
     };
 
-    that.decode64 = function(input) {
+    that.decode64 = function (input) {
         var output = "",
             chr1, chr2, chr3 = "",
             enc1, enc2, enc3, enc4 = "",
@@ -190,8 +171,8 @@ var MinifyJpegAsync = (function()
         var base64test = /[^A-Za-z0-9\+\/\=]/g;
         if (base64test.exec(input)) {
             alert("There were invalid base64 characters in the input text.\n" +
-                  "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
-                  "Expect errors in decoding.");
+                "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+                "Expect errors in decoding.");
         }
         input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
 
@@ -208,10 +189,10 @@ var MinifyJpegAsync = (function()
             buf.push(chr1);
 
             if (enc3 != 64) {
-               buf.push(chr2);
+                buf.push(chr2);
             }
             if (enc4 != 64) {
-               buf.push(chr3);
+                buf.push(chr3);
             }
 
             chr1 = chr2 = chr3 = "";
@@ -222,8 +203,7 @@ var MinifyJpegAsync = (function()
         return buf;
     };
 
-    that.resize = function(img, segments, NEW_SIZE)
-    {
+    that.resize = function (img, segments, NEW_SIZE) {
         var size = that.imageSizeFromSegments(segments),
             width = size[0],
             height = size[1],
@@ -232,32 +212,37 @@ var MinifyJpegAsync = (function()
             scale = parseFloat(newSize) / chouhen,
             newWidth = parseInt(parseFloat(newSize) / chouhen * width),
             newHeight = parseInt(parseFloat(newSize) / chouhen * height);
-
-        var canvas = document.createElement('canvas');
+        var canvas,
+            ctx,
+            srcImg,
+            newCanvas,
+            newCtx,
+            destImg;
+        
+        
+        canvas = document.createElement('canvas');
         canvas.width = width;
         canvas.height = height;
-        var ctx = canvas.getContext("2d");
+        ctx = canvas.getContext("2d");
         ctx.drawImage(img, 0, 0);
-        var srcImg = ctx.getImageData(0, 0, width, height);
+        srcImg = ctx.getImageData(0, 0, width, height);
 
-        var newCanvas = document.createElement('canvas');
-    	newCanvas.width = newWidth;
-    	newCanvas.height = newHeight;
-        var newCtx = newCanvas.getContext("2d");
-    	var destImg = newCtx.createImageData(newWidth, newHeight);
-    	that.bilinear(srcImg, destImg, scale);
+        newCanvas = document.createElement('canvas');
+        newCanvas.width = newWidth;
+        newCanvas.height = newHeight;
+        newCtx = newCanvas.getContext("2d");
+        destImg = newCtx.createImageData(newWidth, newHeight);
+        that.bilinear(srcImg, destImg, scale);
 
-    	newCtx.putImageData(destImg, 0, 0);
+        newCtx.putImageData(destImg, 0, 0);
         return newCanvas.toDataURL("image/jpeg");
     };
 
-    that.getExif = function(segments)
-    {
+    that.getExif = function (segments) {
         var seg;
-        for (var x=0; x<segments.length; x++)
-        {
+        for (var x = 0; x < segments.length; x++) {
             seg = segments[x];
-            if (seg[0] == 255 & seg[1] == 225) //(ff e1)
+            if (seg[0] == 255 && seg[1] == 225) //(ff e1)
             {
                 return seg;
             }
@@ -265,77 +250,75 @@ var MinifyJpegAsync = (function()
         return [];
     }
 
-    that.insertExif = function(imageStr, exifArray)
-    {
+    that.insertExif = function (imageStr, exifArray) {
         var buf = that.decode64(imageStr.replace("data:image/jpeg;base64,", "")),
-            separatePoint = buf.indexOf(255,3),
+            separatePoint = buf.indexOf(255, 3),
             mae = buf.slice(0, separatePoint),
             ato = buf.slice(separatePoint),
-            array = mae.concat(exifArray, ato);
-
-        aBuffer = new Uint8Array(array);
+            array = mae.concat(exifArray, ato),
+            aBuffer = new Uint8Array(array);
 
         return aBuffer;
     };
 
     // compute vector index from matrix one
     that.ivect = function (ix, iy, w) {
-    	// byte array, r,g,b,a
-    	return((ix + w * iy) * 4);
+        // byte array, r,g,b,a
+        return ((ix + w * iy) * 4);
     };
 
+    that.inner = function (f00, f10, f01, f11, x, y) {
+        var un_x = 1.0 - x;
+        var un_y = 1.0 - y;
+        return (f00 * un_x * un_y + f10 * x * un_y + f01 * un_x * y + f11 * x * y);
+    }    
+    
     that.bilinear = function (srcImg, destImg, scale) {
-    	// c.f.: wikipedia english article on bilinear interpolation
-    	//log.debug("in bilinear");
-    	// taking the unit square
-    	function inner(f00, f10, f01, f11, x, y) {
-            var un_x = 1.0 - x; var un_y = 1.0 - y;
-            return (f00 * un_x * un_y + f10 * x * un_y + f01 * un_x * y + f11 * x * y);
-    	}
+        // taking the unit square
         var srcWidth = srcImg.width;
         var srcHeight = srcImg.height;
         var srcData = srcImg.data;
         var dstData = destImg.data;
-    	var i, j;
-    	var iyv, iy0, iy1, ixv, ix0, ix1;
-    	var idxD, idxS00, idxS10, idxS01, idxS11;
-    	var dx, dy;
-    	var r, g, b, a;
-    	for (i = 0; i < destImg.height; ++i) {
+        var i, j;
+        var iyv, iy0, iy1, ixv, ix0, ix1;
+        var idxD, idxS00, idxS10, idxS01, idxS11;
+        var dx, dy;
+        var r, g, b, a;
+        for (i = 0; i < destImg.height; ++i) {
             iyv = (i + 0.5) / scale - 0.5;
             iy0 = Math.floor(iyv);
-            iy1 = ( Math.ceil(iyv) > (srcHeight-1) ? (srcHeight-1) : Math.ceil(iyv) );
-    		for (j = 0; j < destImg.width; ++j) {
+            iy1 = (Math.ceil(iyv) > (srcHeight - 1) ? (srcHeight - 1) : Math.ceil(iyv));
+            for (j = 0; j < destImg.width; ++j) {
                 ixv = (j + 0.5) / scale - 0.5;
                 ix0 = Math.floor(ixv);
-                ix1 = ( Math.ceil(ixv) > (srcWidth-1) ? (srcWidth-1) : Math.ceil(ixv) );
+                ix1 = (Math.ceil(ixv) > (srcWidth - 1) ? (srcWidth - 1) : Math.ceil(ixv));
                 idxD = that.ivect(j, i, destImg.width);
                 idxS00 = that.ivect(ix0, iy0, srcWidth);
                 idxS10 = that.ivect(ix1, iy0, srcWidth);
                 idxS01 = that.ivect(ix0, iy1, srcWidth);
                 idxS11 = that.ivect(ix1, iy1, srcWidth);
-                // log.debug(sprintf("bilinear: idx: D: %d, S00: %d, S10: %d, S01: %d, S11: %d", idxD, idxS00, idxS10, idxS01, idxS11));
-                dx = ixv - ix0; dy = iyv - iy0;
+
+                dx = ixv - ix0;
+                dy = iyv - iy0;
 
                 //r
-                dstData[idxD] = inner(srcData[idxS00], srcData[idxS10],
-                srcData[idxS01], srcData[idxS11], dx, dy);
+                dstData[idxD] = that.inner(srcData[idxS00], srcData[idxS10],
+                    srcData[idxS01], srcData[idxS11], dx, dy);
 
                 //g
-                dstData[idxD+1] = inner(srcData[idxS00+1], srcData[idxS10+1],
-                srcData[idxS01+1], srcData[idxS11+1], dx, dy);
+                dstData[idxD + 1] = that.inner(srcData[idxS00 + 1], srcData[idxS10 + 1],
+                    srcData[idxS01 + 1], srcData[idxS11 + 1], dx, dy);
 
                 //b
-                dstData[idxD+2] = inner(srcData[idxS00+2], srcData[idxS10+2],
-                srcData[idxS01+2], srcData[idxS11+2], dx, dy);
+                dstData[idxD + 2] = that.inner(srcData[idxS00 + 2], srcData[idxS10 + 2],
+                    srcData[idxS01 + 2], srcData[idxS11 + 2], dx, dy);
 
                 //a
-                dstData[idxD+3] = inner(srcData[idxS00+3], srcData[idxS10+3],
-                srcData[idxS01+3], srcData[idxS11+3], dx, dy);
+                dstData[idxD + 3] = that.inner(srcData[idxS00 + 3], srcData[idxS10 + 3],
+                    srcData[idxS01 + 3], srcData[idxS11 + 3], dx, dy);
 
-    			// log.debug(sprintf("pixel: j:%d, i:%d; r:%d, g:%d, b:%d", j, i, r, g, b));
-    		}
-    	}
+            }
+        }
     };
     return that;
 })();
